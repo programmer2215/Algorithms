bcc161acc607759934fed2f9a57eee11a26c94a8
Mark Madhukar <markmadhukar03@gmail.com>
2021-07-11 12:49:47 +0530

    added diagonalDifference.py

---
+++ diagonalDifference.py
@@ -0,0 +1,23 @@
+array_input = [
+    [3, 2, 5, 6, 10],
+    [5, 6, 7, 6, 9],
+    [2, 9, 7, 5, 8],
+    [3, 2, 7, 5, 5],
+    [5, 6, 7, 5, 7]
+]
+
+def diagonal_diff(array):
+    l_diagonal, r_diagonal = 0,0
+    for i in range(len(array)):
+        
+        l_diagonal += array[i][i]
+    for j in range(0, len(array)):
+        
+        r_diagonal += array[j][0-(j + 1)]
+    
+    diff = abs(l_diagonal - r_diagonal)
+
+    return diff
+
+
+print(diagonal_diff(array_input))
