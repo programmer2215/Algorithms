9801b5a3cbe68b729a4bfbf09f5c36e995599ca1
Mark Madhukar <markmadhukar03@gmail.com>
2021-07-14 18:32:45 +0530

    added balancedBrackets.py

---
+++ balancedBrackets.py
@@ -0,0 +1,29 @@
+
+inp_1 = "[{{([])}}]"
+inp_2 = ")([({}){})"
+
+
+
+def isBalanced(s):
+    # pretend stack
+    
+    stack = []
+
+    for i in range(len(s)):
+        if s[i] in  ["(", "{", "["]:
+            stack.append(s[i])
+        else:
+            if len(stack) == 0:
+                return "NO"
+            else:
+                pop_val = stack.pop()
+                if pop_val == "(" and s[i] != ")":
+                    return "NO"
+                elif pop_val == "{" and s[i] != "}":
+                    return "NO"
+                elif pop_val == "[" and s[i] != "]":
+                    return "NO"
+    return "YES"
+
+print(isBalanced(inp_1))
+print(isBalanced(inp_2))
